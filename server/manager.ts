ede41a2037575a86702b22d18b9f88df6e08617c
陆拾遗 <iwof920125@hotmail.com>
2021-04-18 12:53:10 +0800

    开发中

--- server/manager.ts
+++ server/manager.ts
@@ -2,8 +2,7 @@ import log4js from "common-log4js";
 import { basename } from "path";
 const logger = log4js.getLogger(basename(__filename));
 ///////////////////////////////////////////////////////
-import Express = require("express");
-import bodyParser = require('body-parser');
+import { http_serv_start, app } from "common-https";
 import { SERVER_REQUEST, IServerReq, IFindReq, HttpReturn } from "common-manager";
 import { create_server_info, get_server_info, get_all_server_info } from "../manager/serverMgr";
 
@@ -14,28 +13,13 @@ function http_return(res: any, ret: HttpReturn) {
     res.send(httpReturn);
 }
 
-const app = Express();
-app.use(bodyParser.json());
 export function server_start(http_ip: string, http_port: number) {
-    //设置跨域访问
-    app.all('*', (req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-        res.header("X-Powered-By", ' 3.2.1')
-        res.header("Content-Type", "application/json;charset=utf-8");
-        logger.debug("[%s] %s => %s", req.method, req.path, JSON.stringify(req.body));
-        next();
-    });
-
-    app.listen(http_port, http_ip);
-    logger.info("Find Server Running at %s:%s", http_ip, http_port);
+    http_serv_start(http_ip, http_port);
 }
 
 // 服务注册
 app.post(SERVER_REQUEST.REPORT, (req, res) => {
     const server_info: IServerReq = req.body;
-    logger.debug(server_info);
     create_server_info(server_info);
     http_return(res, {});
 })
